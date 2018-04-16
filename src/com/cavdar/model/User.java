f9bcdc7d563c331bc1fe8fe1f2956352fceb922a
cavdarburak <cavdarburak@gmail.com>
2018-04-16 17:05:02 +0300

    constructor change

--- src/com/cavdar/model/User.java
+++ src/com/cavdar/model/User.java
@@ -2,28 +2,37 @@ package com.cavdar.model;
 
 public class User {
   private String name;
-  private int age;
 
-  public String getName() {
-    return name;
+  private int age;
+  public User() {
+    super();
   }
 
-  public void setName(String name) {
+  public User(String name, int age) {
+    super();
     this.name = name;
+    this.age = age;
   }
 
   public int getAge() {
     return age;
   }
 
+  public String getName() {
+    return name;
+  }
+
   public void setAge(int age) {
     this.age = age;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   @Override
   public String toString() {
     return "User [name=" + name + ", age=" + age + "]";
   }
-  
-  
+
 }
